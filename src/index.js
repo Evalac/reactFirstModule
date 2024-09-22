55559b2c88e7cff2177d3313e4c7eedb6c92b943
Evalac <maslenokprograming@gmail.com>
2024-09-22 15:05:20 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -2,9 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import datas from './';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// {const elemr = <span>HELLO</span>;
+// const elema = <span>WORLD</span>;
+
+// const jsxElement = (
+//   <div className="react">
+//     {elemr}
+//     {elema}
+//   </div>
+// );
+// console.log(jsxElement);
+
+// ReactDOM.createRoot(document.querySelector('#root')).render(jsxElement);
+// }
+
+const data = {
+  id: 1,
+  name: 'Смартфон Apple iPhone 13',
+  description:
+    'Потужний процесор A15 Bionic і чудова камера для фото та відео.',
+  price: 999.99,
+  image: 'https://via.placeholder.com/200x150?text=iPhone+13',
+};
+
+const painting = (
+  <div className="product-card">
+    <img src={data.image} alt={data.name} className="product-image" />
+    <div className="product-info">
+      <h3 className="product-name">{data.name}</h3>
+      <p className="product-description">{data.description}</p>
+      <p className="product-price">Price: {data.price}</p>
+      <button className="add-to-cart-btn">Додати в кошик</button>
+    </div>
+  </div>
 );
+
+console.log(painting);
+ReactDOM.createRoot(document.querySelector('#root')).render(painting);
