d7bc6fee5a185a8a38b129e776381137d40aebab
Evalac <maslenokprograming@gmail.com>
2024-09-23 17:32:13 +0300

    gaddjets list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,10 @@
-import Gaddjets from './Gaddjets';
+import GaddjetList from './GaddjetsList';
 import data from '../data';
 
 export const App = () => {
-  const isOnline = true; // рендер по умові
-  return isOnline ? (
+  return (
     <div>
-      {data.map(gaddjet => (
-        <Gaddjets
-          key={gaddjet.id}
-          url={gaddjet.image}
-          title={gaddjet.name}
-          description={gaddjet.description}
-          price={gaddjet.price}
-          quantity={gaddjet.quantity}
-        />
-      ))}
+      <GaddjetList items={data} />
     </div>
-  ) : (
-    'ofline'
   );
 };
--- src/components/GaddjetsList.jsx
+++ src/components/GaddjetsList.jsx
@@ -1,1 +1,21 @@
-function GaddjetList(params) {}
+import Gaddjets from './Gaddjets';
+
+function GaddjetList({ items }) {
+  return (
+    <ul>
+      {items.map(item => (
+        <li key={item.id}>
+          <Gaddjets
+            url={item.image}
+            title={item.name}
+            description={item.description}
+            price={item.price}
+            quantity={item.quantity}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default GaddjetList;
